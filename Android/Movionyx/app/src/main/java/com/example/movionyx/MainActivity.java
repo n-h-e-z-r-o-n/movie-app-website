6f062cc4849f39e3cdc4730b175c4100bc5a970b
Hezron Na <hezron.w12@gmail.com>
2025-02-02 17:53:58 +0300

    Committed 2025 0Su.175358

---
+++ Android/Movionyx/app/src/main/java/com/example/movionyx/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.movionyx;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.xvr);
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript if needed
+        webSettings.setDomStorageEnabled(true); // Enable local storage if needed
+        webView.setWebViewClient(new WebViewClient()); // Ensures links open in WebView
+
+        //webView.loadUrl("https://movionyx.com"); // Replace with your URL
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("https://movionyx.com")) {
+                    return false; // Allow loading
+                } else {
+                    Toast.makeText(MainActivity.this, "Redirect Blocked!", Toast.LENGTH_SHORT).show();
+                    return true; // Block redirect
+                }
+            }
+        });
+        webView.loadUrl("https://movionyx.com"); // Change to your website
+
+    }
+}
