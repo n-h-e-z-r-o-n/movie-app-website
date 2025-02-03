43bf414a116a57d5a23903721dda476a4adeacbb
Hezron Na <hezron.w12@gmail.com>
2025-02-03 20:10:50 +0300

    Committed 2025 0Mo.201049

--- Android/Movionyx/app/src/main/java/com/example/movionyx/MainActivity.java
+++ Android/Movionyx/app/src/main/java/com/example/movionyx/MainActivity.java
@@ -8,9 +8,13 @@ import android.widget.VideoView;
 import android.webkit.WebViewClient;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    // List of ad-related domains to block
+    private final Set<String> adHosts = new HashSet<>();
 
 
     @Override
@@ -67,5 +71,33 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed(); // Exit app if no history
         }
     }
+
+
+
+    // Initialize ad blocker by adding common ad domains
+    private void initializeAdBlocker() {
+        adHosts.add("googleadservices.com");
+        adHosts.add("doubleclick.net");
+        adHosts.add("ads.yahoo.com");
+        adHosts.add("adservice.google.com");
+        adHosts.add("admob.com");
+        adHosts.add("pagead2.googlesyndication.com");
+        adHosts.add("securepubads.g.doubleclick.net");
+        adHosts.add("adserver.snapads.com");
+        adHosts.add("ads.twitter.com");
+        adHosts.add("ads.facebook.com");
+    }
+
+    // Check if a URL is from an ad provider
+    private boolean isAdUrl(String url) {
+        for (String host : adHosts) {
+            if (url.contains(host)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
