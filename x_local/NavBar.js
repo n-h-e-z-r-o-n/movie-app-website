1b3d93d2ac524d81ce32fa47ab65796caf0e393d
Hezron Na <hezron.w12@gmail.com>
2025-05-19 23:38:33 +0300

    Committed 2025 0Mo.233833

--- x_local/NavBar.js
+++ x_local/NavBar.js
@@ -326,6 +326,8 @@ async function PlayTrailer(id_play, type){
 
 
 async function AddToFav(movie, widget){
+
+    widget.classList.remove('button_style2');
     widget.classList.add('button_style3');
     return
     let Fave = localStorage.getItem('user_watchlist');
