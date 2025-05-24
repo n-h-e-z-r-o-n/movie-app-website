3378d6d24571892002bf30d80a38bc0f5097f9cf
Hezron Na <hezron.w12@gmail.com>
2025-05-24 14:50:47 +0300

    Committed 2025 0Sa.145047

--- Source_code/test2.py
+++ Source_code/test2.py
@@ -12,11 +12,5 @@ def on_before_load(window, url):
 if __name__ == '__main__':
     window = webview.create_window('Block Redirect Example', 'https://movionyx.com')
     window.events.before_load += on_before_load
-    webview.settings = {
-        'ALLOW_DOWNLOADS': False,
-        'ALLOW_FILE_URLS': True,
-        'OPEN_EXTERNAL_LINKS_IN_BROWSER': True,
-        'OPEN_DEVTOOLS_IN_DEBUG': True,
-        'REMOTE_DEBUGGING_PORT': None
-    }
+
     webview.start(user_agent="MovionyxApp/1.0")
