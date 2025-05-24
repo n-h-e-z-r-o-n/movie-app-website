c70e5d997a77cb1ffb1de2d35cc6d8fdec8bb25a
Hezron Na <hezron.w12@gmail.com>
2025-05-24 08:16:34 +0300

    Committed 2025 0Sa. 81634

--- Source_code/test2.py
+++ Source_code/test2.py
@@ -1,5 +1,7 @@
 import webview
 
 if __name__ == '__main__':
-    webview.create_window('', 'https://movionyx.com', frameless=True,easy_drag=True, fullscreen=True, )
-    webview.start(user_agent='MovionyxApp/1.0', icon='./logo.png')
+    webview.settings['OPEN_EXTERNAL_LINKS_IN_BROWSER'] = False
+    webview.settings['OPEN_DEVTOOLS_IN_DEBUG'] = False
+    webview.create_window('', 'https://movionyx.com', frameless=False, easy_drag=True, fullscreen=False, )
+    webview.start(user_agent='MovionyxApp/1.0', icon='/logo.png')
