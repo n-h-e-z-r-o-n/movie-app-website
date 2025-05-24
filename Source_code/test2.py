c1fe708e7c3f791ca06599b59f2fe273485a61f8
Hezron Na <hezron.w12@gmail.com>
2025-05-24 15:00:56 +0300

    Committed 2025 0Sa.150056

--- Source_code/test2.py
+++ Source_code/test2.py
@@ -1,16 +1,22 @@
 import webview
 
-def on_before_load(window, url):
+def on_before_load(window, request):
     # Block specific URLs (e.g., containing "redirect" or a specific domain)
+    url = request.url  # Access the URL from the request object
     if "https://movionyx.com" in url:
         return True  # Allow navigation
     else:
-        print(f"Blocked redirect ")
+        print(f"Blocked redirect: {url}")
         return False  # Cancel navigation
-
 
 if __name__ == '__main__':
     window = webview.create_window('Block Redirect Example', 'https://movionyx.com')
     window.events.before_load += on_before_load
-
+    window.webview.settings = {
+        'ALLOW_DOWNLOADS': False,
+        'ALLOW_FILE_URLS': True,
+        'OPEN_EXTERNAL_LINKS_IN_BROWSER': False,
+        'OPEN_DEVTOOLS_IN_DEBUG': True,
+        'REMOTE_DEBUGGING_PORT': None
+    }
     webview.start(user_agent="MovionyxApp/1.0")
